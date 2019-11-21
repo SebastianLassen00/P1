69bf05a322c051e11cb47701251a5ef6b516766d
MagnusSandAndersen <msan19@student.aau.dk>
2019-11-21 11:06:23 +0100

    lengthOfVector written but not tested

--- include/vectors.h
+++ include/vectors.h
@@ -4,7 +4,7 @@
 void copyVector(const double v[], int size, double out[]);
 void addVector(const double v1[], const double v2[], int size, double sum[]);
 void scaleVector(const double v[], double scale, int size, double out[]);
-double lengthOfVector(const double v[]);
+double lengthOfVector(const double v[], int size);
 void normalizeVector(const double v[], double out[]);
 double dotProduct(const double v1[], const double v2[]);
 
