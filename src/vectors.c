1d0b1c6cdc6521d41c75efb7392d23b0ddeb5d6f
Simonbolsen <Simonbolsen1234@gmail.com>
2019-11-21 10:51:35 +0100

    Added test vector for dotproduct

--- src/vectors.c
+++ src/vectors.c
@@ -4,6 +4,7 @@
 int main(void){
     double vector[3] = {1, 0.5, 0.1};
     double vectorTest[3] = {0.2, 0.3, 0.0};
+    double vector_dot_test[3] = {0.0 , 0.0, 1.1};
     
     copyVector(vector, 3, vectorTest);
     printVector(vectorTest, 3);
