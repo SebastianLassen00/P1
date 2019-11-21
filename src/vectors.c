0b38614c4b241a40f0fabca93bb5e823cf6f8398
sebastianlassen00 <slasse19@student.aau.dk>
2019-11-21 09:44:55 +0100

    vectors.c

---
+++ src/vectors.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "vectors.h"
+
+int main(void){
+
+}
+
+void copyVector(const double in[], int size, double out[]){
+    int i;
+
+    for(i = 0; i < size; i++)
+        out[i] = in[i];
+    
+
+}
