af1b973235e652027cf2d54128bd7b2302658343
sebastianlassen00 <slasse19@student.aau.dk>
2019-11-22 17:16:17 +0100

    Vector struct

--- include/vectors.h
+++ include/vectors.h
@@ -1,6 +1,12 @@
 #ifndef VECTORS_H
 #define VECTORS_H
 
+struct vector{
+    double *array;
+    int number_of_interests;
+};
+
+
 void copyVector(const double v[], int size, double out[]);
 void addVector(const double v1[], const double v2[], int size, double sum[]);
 void subtractVector(const  double v1[], const double v2[], int size, double sum[]);
