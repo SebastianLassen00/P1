15ba788015dc6a21e4d1fab449f9d991f38cc2b2
Steven <thedankpure@gmail.com>
2019-11-25 10:21:57 +0100

    added name parsing

--- include/parser.h
+++ include/parser.h
@@ -1,9 +1,12 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include<stdio.h>
-#include"database.h"
+#include <stdio.h>
+#include "database.h"
 
-void parseDatabase(struct database *, FILE *);
+void parseDatabase(struct database *database, FILE *filereader);
+int parseNumOfEdu(FILE *filereader);
+void parseEduNames(struct education *education, int amount_of_educations, FILE *filereader);
+char *parseEduString(char* current_line, int amount_of_educations, char* education_string, int offset);
 
 #endif
