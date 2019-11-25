ff5ca445118708bc3a5d47b061cd6d42153980f0
ChBLA <cbla19@student.aau.dk>
2019-11-25 14:17:32 +0100

    Update test_main.c

--- src/test_main.c
+++ src/test_main.c
@@ -78,7 +78,9 @@ void chooseFromList(struct profile user, int interval_start, int interval_end){
     char temp_string[MAX_INPUT_LENGTH];
  
     do{
-        scanf(" %d %c", &temp_subject, &temp_char);
+        scan_res = scanf(" %d%c", &temp_subject, &temp_char);
+        if(scan_res == 0) 
+            scanf(" %c", temp_char);
         printf("%d:%c\n", temp_subject, temp_char);
         printf("%d: %d\n", levelAsValue(temp_char), (int) levelAsValue(temp_char));
         if(temp_subject > 0 && temp_subject < (interval_end - interval_start + 1) && levelAsValue(temp_char) != -1) {
