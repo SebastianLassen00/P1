094930a51ea82620e4dc35237cb988590260f0bd
ChBLA <cbla19@student.aau.dk>
2019-11-25 11:25:22 +0100

    Worked on test_main

--- include/profile.h
+++ include/profile.h
@@ -1,13 +1,14 @@
 #ifndef PROFILE_H
 #define PROFILE_H
 
+#define MAX_NAME_LENGTH 20
+
 struct profile{
 	struct vector interests;
 	char * name;
 	struct qualifications qualifications;
 	double average;
 	struct location location;
-	struct vector ;
 
 };
 
