466e18005c7f0a09916d82e4d90ac3d6359716ba
msan19 <msan19@student.aau.dk>
2019-11-29 14:23:54 +0100

    included header files that is in main

---
+++ test/AllTests.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <profile.h>
+#include <education.h>
+#include <subjects.h>
+#include <vector.h> 
+
+int main(){
+
+    return 0;
+}
