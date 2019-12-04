88a17305320000e4b7ad755e45409ad6dc9c9ccf
Steven <thedankpure@gmail.com>
2019-12-04 14:42:42 +0100

    bobo??

--- test/AllTests.c
+++ test/AllTests.c
@@ -28,6 +28,27 @@ void RunAllTests(void){
     printf("%s\n", output->buffer); 
 }
 
+struct profile createBobo(int amount_of_interests){
+    struct profile bobo = createProfile(amount_of_interests);
+    int i;
+    bobo.average = 15.7;
+    strcpy(bobo.name, "Bobo");
+    bobo.location.region = 0;
+    bobo.location.region_importance = 0.1;
+
+    for(i = 0; i < amount_of_interests; i++){
+        bobo.interests.array[i] = 0.0;
+    }
+
+    for(i = 0; i < amount_of_interests; i++){
+        bobo.qualifications.subjects[i].level = 3;
+    }
+
+    setSubjects(&bobo);
+
+    return bobo;
+}
+
 int main(void){
     RunAllTests();
 
