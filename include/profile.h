9ee29ba762fcafea2e89f164ca645024f4f6af06
msan19 <msan19@student.aau.dk>
2019-12-10 15:32:41 +0100

    documentation skeleton for profile struct

--- include/profile.h
+++ include/profile.h
@@ -13,6 +13,21 @@
 #ifndef PROFILE_H
 #define PROFILE_H
 
+/**
+ * @struct profile
+ * @brief Describes a user.
+ * 
+ * This structure defines a user and all the details about the user.
+ * @var interests 
+ * @var adjustment_vector 
+ * @var name 
+ * @var qualifications
+ * @var average 
+ * @var location 
+ * @var saved_educations 
+ * @var last_recommended
+ * @var recommended_educations
+ */
 struct profile{
     struct vector interests;
     struct vector adjustment_vector;
